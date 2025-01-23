62274529a8025dc4c0b9adef81f8c44ab91c63c0
John Doe <johndoe@email.com>
2025-01-23 17:17:48 -0600

    Problem 1

--- Easy/Two_sum.py
+++ Easy/Two_sum.py
@@ -1,2 +1,7 @@
 class Solution(object):
   def twoSum(self, nums, target): 
+    for k in range(len(nums)):
+      for i in range(k+1, len(nums)):
+        if nums[k] + nums[i] == target:
+             return [k, i]
+    return []
